e42008ac44340cbc56f7b5a0c3e0ae5f3c6e0845
Chris-Owusu <christopher.owusu@amalitech.com>
2023-11-15 16:25:22 +0000

    first test

--- Java/src/test/java/AppiumBasic/BaseTest.java
+++ Java/src/test/java/AppiumBasic/BaseTest.java
@@ -1,5 +1,49 @@
 package AppiumBasic;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
 public class BaseTest {
+	public AndroidDriver driver;
+	AppiumDriverLocalService service;
+	
+	
+	@BeforeClass
+	public void ConfigureAppium() throws MalformedURLException {
+		
+		// Automatically start service.
+		service = new AppiumServiceBuilder().withAppiumJS(new File("C://Users//ChristopherOwusuAhen//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
+					.withIPAddress("127.0.0.1").usingPort(4723).build();
+		service.start();
+		
+		
+		// Setting up UIAutomator
+		UiAutomator2Options option = new UiAutomator2Options();
+		option.setDeviceName("Pixel 7 Pro");
+		option.setApp("C://eclipseWorkflow//Java//Java//src//test//java//resources//ApiDemos-debug.apk");
+		
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), option);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+	}
+	
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+		service.stop();
+	}
 
 }
