c24b1966766d25b9d976abff7cd187525cbbe6b1
Chris-Owusu <christopher.owusu@amalitech.com>
2023-11-24 14:28:25 +0000

    open browser on mobile device

--- Java/src/test/java/GStore/MobileBrowserTest.java
+++ Java/src/test/java/GStore/MobileBrowserTest.java
@@ -1,17 +1,22 @@
 package GStore;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MobileBrowserTest extends BrowserBaseTest {
 	
 	@Test
 	public void browserTest() throws InterruptedException {
-		driver.get("https://google.com");
-		driver.findElement(By.name("q")).sendKeys("Mangoes");
-		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-		Thread.sleep(3000);
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.cssSelector(".navbar-toggler-icon")).click();
+		driver.findElement(By.cssSelector(".nav-link[routerlink='/products']")).click();
+		((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 1000)", "");
+		String devops = driver.findElement(By.xpath("//a[normalize-space()='Devops']")).getText();
+		Assert.assertEquals(devops, "Devops");
+		
+		
 	}
 
 }
