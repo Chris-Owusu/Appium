ffdad0b1e7ed695c4b0de708663d91bda4f14b89
Chris-Owusu <christopher.owusu@amalitech.com>
2023-11-27 17:28:00 +0000

    swipe

--- Java/src/test/java/IOS/IOSBasis.java
+++ Java/src/test/java/IOS/IOSBasis.java
@@ -10,7 +10,10 @@ public class IOSBasis {
 		// LongPress --> "mobile:touchAndHold"
 		// For dropdowns --> we use sendkeys
 		// Slider --> use sendKeys. and it's between 0% - 1%
+		// loadApp/install app --> "mobile:launchApp", the bundle ID is "com.apple.mobileslideshow", "mobile:swipe"
+		// Swipe --> "mobile:swipe"
 		
+		// Go back --> driver.navigate().back();
 	}
 
 }
