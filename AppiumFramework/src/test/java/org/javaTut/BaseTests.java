75099805e85d8daace7742923980074a0d85fd59
Chris-Owusu <christopher.owusu@amalitech.com>
2023-12-06 09:48:05 +0000

    resources --> properties file

--- AppiumFramework/src/test/java/org/javaTut/BaseTests.java
+++ AppiumFramework/src/test/java/org/javaTut/BaseTests.java
@@ -1,9 +1,11 @@
 package org.javaTut;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.javaTut.AppiumFramework.pageObjects.android.FormPage;
 import org.javaTut.AppiumFramework.utils.AndroidActions;
@@ -27,17 +29,23 @@ public class BaseTests extends AndroidActions {
 	public FormPage formPage;
 	
 	@BeforeClass
-	public void ConfigureAppium() throws MalformedURLException {
+	public void ConfigureAppium() throws IOException {
 		
 		// Automatically start service.
 		service = new AppiumServiceBuilder().withAppiumJS(new File("C://Users//ChristopherOwusuAhen//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
 					.withIPAddress("127.0.0.1").usingPort(4723).build();
 		service.start();
 		
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//main//java//org//javaTut//AppiumFramework//resources//data.properties");
+		prop.load(fis);
+		
+		String phone = prop.getProperty("deviceName");
+		
 		
 		// Setting up UIAutomator
 		UiAutomator2Options option = new UiAutomator2Options();
-		option.setDeviceName("GStore"); // emulator
+		option.setDeviceName(phone); // emulator
 //		option.setDeviceName("Android Device");   // When running it on a real device
 		option.setChromedriverExecutable("C://Users//ChristopherOwusuAhen//Downloads//chromedriver.exe");
 		option.setApp("C://eclipseWorkflow//Java//Java//src//test//java//resources//General-Store.apk");
