3d0d747ba6f075883ee955017a1348ccc2381c06
Chris-Owusu <christopher.owusu@amalitech.com>
2023-12-12 17:24:58 +0000

    working with date in Java

---
+++ Java/src/test/java/javaTut/Date.java
@@ -0,0 +1,19 @@
+package javaTut;
+
+import java.text.SimpleDateFormat;
+
+public class Date {
+	
+	public static void main(String[] args) {
+		Date d = new Date();
+		
+		System.out.println(d.toString());
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+		System.out.println(sdf.format(d));
+		
+		SimpleDateFormat sd = new SimpleDateFormat("M/d/yyyy hh:mm:ss");
+		System.out.println(sd.format(d));
+	}
+
+}
