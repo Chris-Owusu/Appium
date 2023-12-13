557768ae8cda1b4a208e357edb2264de502bc8ca
Chris-Owusu <christopher.owusu@amalitech.com>
2023-12-13 08:38:48 +0000

    fixed date and calender script

--- Java/src/test/java/javaTut/Calender.java
+++ Java/src/test/java/javaTut/Calender.java
@@ -1,5 +1,16 @@
 package javaTut;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Calender {
+	
+	public static void main(String[] args) {
+		
+		Calendar cal = Calendar.getInstance();
+		
+		SimpleDateFormat sd = new SimpleDateFormat("M/d/yyyy hh:mm:ss");
+		System.out.println(sd.format(cal.getTime()));
+	}
 
 }
--- Java/src/test/java/javaTut/Date.java
+++ Java/src/test/java/javaTut/Date.java
@@ -3,17 +3,15 @@ package javaTut;
 import java.text.SimpleDateFormat;
 
 public class Date {
-	
-	public static void main(String[] args) {
-		Date d = new Date();
-		
-		System.out.println(d.toString());
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
-		System.out.println(sdf.format(d));
-		
-		SimpleDateFormat sd = new SimpleDateFormat("M/d/yyyy hh:mm:ss");
-		System.out.println(sd.format(d));
-	}
 
+    public static void main(String[] args) {
+        java.util.Date d = new java.util.Date();
+        System.out.println(d.toString());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+        System.out.println(sdf.format(d));
+
+        SimpleDateFormat sd = new SimpleDateFormat("M/d/yyyy hh:mm:ss");
+        System.out.println(sd.format(d));
+    }
 }
